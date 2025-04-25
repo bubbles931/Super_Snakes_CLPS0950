65db71c116f02cd46824f017b3aee3cae85a521c
bubbles931 <hannah_stonorov@brown.edu>
2025-04-25 11:07:30 -0400

    Adding comments
    Adding comments line by line

--- super_snakes.py
+++ super_snakes.py
@@ -1,22 +1,30 @@
+#import pygame
 import pygame as pgame
 #define function to create board
 def board():
-  #initialize 
+  #initialize all pygame functions
   pgame.init()
+  #creates display window with parameters
   stage = pgame.display.set_mode((1000,400))
 
   running = True
 
+#main game loop 
   while running:
+    #loops through possible user interactions
     for event in pgame.event.get():
+      #if user presses close button exits loop and display closes
       if event.type == pgame.QUIT:
         running = False
-
+    
+    #clearing screen
     stage.fill((255, 255, 255))
 
+    #creating rectangle and defining position
     pgame.draw.rect(stage, (150,150, 150), pgame.Rect(150,100,100,150))
     pgame.display.flip()
 
   pgame.quit()
-
+  
+#calling function
 board()
