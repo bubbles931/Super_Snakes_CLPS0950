f967e67dacc38ddcf6a9e57377b8b7ef1e262d08
mickey0116 <144743300+mickey0116@users.noreply.github.com>
2025-04-28 21:11:08 -0400

    created python file to create snake
    added a description and objective of snake_icon.py

---
+++ snake_icon.py
@@ -0,0 +1,7 @@
+#will be using:
+#   pygame.draw to create a snake 
+#   pygame.event to assocaote keyboard moves w display moves
+#   after this: potientally create a function that can get the snakes location 
+#       to be called in game_logic.py or board.py to NOT populate food in the same boardsquare
+def create_snake():
+    print("dumby line")
