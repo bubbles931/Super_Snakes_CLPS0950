3ddfa890b7e7e6e1da487962b98664f83f1f3a70
mickey0116 <144743300+mickey0116@users.noreply.github.com>
2025-04-30 16:00:08 -0400

    cont.
    when adding to body, color will be alternating

--- snake_icon.py
+++ snake_icon.py
@@ -5,7 +5,15 @@
 #       to be called in game_logic.py or board.py to NOT populate food in the same boardsquare
 
 import pygame
+counter = 1
+body_list = []
 
 def create_snake(stage, movement_rows, movement_cols, center):
     head = pygame.draw.circle(stage, (255, 255, 0,), center, 10)
-    body = pygame.draw.rect(stage,(255,0,127), pygame.Rect((movement_rows-1)*25,(movement_cols)*25,25,25))
+    if counter != 0:
+        if counter % 2 == 0:
+            body = pygame.draw.rect(stage,(255,170,29), pygame.Rect((movement_rows-1)*25,(movement_cols)*25,25,25))
+            body_list.append(body)
+        else:
+            body = pygame.draw.rect(stage,(255,0,127), pygame.Rect((movement_rows-1)*25,(movement_cols)*25,25,25))
+            body_list.append(body)
