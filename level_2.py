5edfe15f5da69eed93b1dfb394e314e1e47a3beb
mickey0116 <144743300+mickey0116@users.noreply.github.com>
2025-05-07 21:21:32 -0400

    attempting to change logic

--- level_2.py
+++ level_2.py
@@ -1,6 +1,9 @@
 #this is the file for level 2
 start_level_2 = False
 
+def main(stage):
+    stage.fill(100,100,100)
+    
 def level_2():
     if start_level_2 == True:
         print('your mama')
